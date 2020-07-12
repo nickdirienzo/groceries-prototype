d3b4c14fc0c04783685ab9a10f0f045f803e4474
Nick DiRienzo <me@nickdirienzo.com>
2020-07-12 16:53:57 -0700

    save the gen'ed grocery list

--- groceries/list_maker.py
+++ groceries/list_maker.py
@@ -1,17 +1,19 @@
 """Idk what else to call this. They make the grocery list."""
+import json
 import os
 from collections import namedtuple
 
 import yaml
 
 GROCERIES_CONFIG = os.environ.get("GROCERIES_CONFIG")
+GROCERY_LIST_ARCHIVE = os.environ.get("GROCERIES_LIST_ARCHIVE")
 
 ShoppingListEntry = namedtuple("ShoppingListEntry", ["name", "quantity"])
 
 with open(GROCERIES_CONFIG) as _file:
     possible_groceries = yaml.full_load(_file)
 
-def get_grocery_list():
+def get_grocery_list(dd_mm_yyyy: str):
     grocery_list = []
     for group, group_config in possible_groceries.items():
         food_options = [
@@ -20,4 +22,11 @@ def get_grocery_list():
         ]
         cadence = group_config["cadence"]
         grocery_list.append(food_options[0])
+
+    # Save for history.
+    with open(os.path.join(GROCERY_LIST_ARCHIVE, f"{dd_mm_yyyy}.json"), "w") as _file:
+        _file.writelines([json.dumps(grocery_list)])
+
     return grocery_list
+
+get_grocery_list("07-12-2020")
